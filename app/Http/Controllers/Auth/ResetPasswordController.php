fe56e422af2e4ddaae0eb8b1bfb82f566107ec8a
tanyu <15290609190@163.com>
2018-09-07 18:47:09 +0800

    完成修改密码发送邮箱验证

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -1,33 +1,38 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Notification;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ResetsPasswords;
 
-class ResetPassword extends Notification
+class ResetPasswordController extends Controller
 {
-    public $token;
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset requests
+    | and uses a simple trait to include this behavior. You're free to
+    | explore this trait and override any methods you wish to tweak.
+    |
+    */
 
-    public function __construct($token)
+    use ResetsPasswords;
+
+    /**
+     * Where to redirect users after resetting their password.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
     {
-        $this->token = $token;
+        $this->middleware('guest');
     }
-
-    public function via($notifiable)
-    {
-        return ['mail'];
-    }
-
-    public function toMail($notifiable)
-    {
-        return (new MailMessage)
-            ->subject('重置密码')
-            ->line('这是一封密码重置邮件，如果是您本人操作，请点击以下按钮继续：')
-            ->action('重置密码', url(route('password.reset', $this->token, false)))
-            ->line('如果您并没有执行此操作，您可以选择忽略此邮件。');
-    }
-
 }
